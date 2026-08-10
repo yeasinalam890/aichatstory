a774d8c218514d8b559d794cf90226b1bc8d3902
Yeasin <yeasinalam890@gmail.com>
2026-08-10 13:02:29 +0530

    Delete engines directory

--- engines/AdminEngine.js
+++
@@ -1,6 +0,0 @@
-class AdminEngine {
-    static authenticate(username, password) {
-        return username === "MdYeasinAlam" && password === "25801@Yeasin";
-    }
-}
-module.exports = AdminEngine;
--- engines/StoryEngine.js
+++
@@ -1,8 +0,0 @@
-class StoryEngine {
-    constructor(storyDetails = {}) {
-        this.scene = storyDetails.prompt || "Opening scene conversation";
-        this.location = storyDetails.social || "Atmospheric Setting";
-    }
-    updateStoryState() { return { scene: this.scene, location: this.location }; }
-}
-module.exports = StoryEngine;
