9f221ec867c1d41ed9faa7f35b0be30c7ee537a0
Yeasin <yeasinalam890@gmail.com>
2026-08-10 13:41:37 +0530

    Update StoryEngine.js

--- StoryEngine.js
+++ StoryEngine.js
@@ -1,8 +1,26 @@
 class StoryEngine {
-    constructor(storyDetails = {}) {
-        this.scene = storyDetails.prompt || "Opening scene conversation";
-        this.location = storyDetails.social || "Atmospheric Setting";
+    constructor(storyDetails) {
+        this.storyDetails = storyDetails || {};
     }
-    updateStoryState() { return { scene: this.scene, location: this.location }; }
+
+    getInitialOpening() {
+        return [
+            {
+                sender: 'assistant',
+                text: "*Che mahine beet gaye, Aisha... tum sach mein aa gaye! Maine socha shayad tum wahan ki chaka-chondh mein mujhe bhool gaye hoge.*\n\n*Usne rote hue Aarav ko gale laga liya, ubh ke beech ki har doori khatam ho chuki thi.*"
+            },
+            {
+                sender: 'assistant',
+                text: "Aisha: \"Main bhi mujhe kal dusri jagah jaana lekin main proof kr ke rahunga. Issed liya abhi mein tumse Direct 6 mahine baat tumhari papa ke sath milunga. Abhi ke liye idhar pause krte hain,promise mein lot ke aunga.\""
+            }
+        ];
+    }
+
+    updateStoryState() {
+        return {
+            scene: this.storyDetails.scene || "Royal Crest University Campus - Dramatic Encounter"
+        };
+    }
 }
+
 module.exports = StoryEngine;
