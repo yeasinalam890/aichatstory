fa2bfede275caba25287cea2e39c5850b5abd0cf
Yeasin <yeasinalam890@gmail.com>
2026-08-10 13:24:24 +0530

    Update LanguageEngine.js

--- LanguageEngine.js
+++ LanguageEngine.js
@@ -2,9 +2,9 @@ class LanguageEngine {
     static detectAndFormatInstruction(userMessage, preference = "Auto") {
         let detected = preference;
         if (preference === "Auto") {
-            if (/[अ-ह]/.test(userMessage)) detected = "Hindi";
-            else if (/[অ-হ]/.test(userMessage)) detected = "Bengali";
-            else if (/[ٱ-ي]/.test(userMessage)) detected = "Urdu";
+            if (/[\u0900-\u097F]/.test(userMessage)) detected = "Hindi";
+            else if (/[\u0980-\u09FF]/.test(userMessage)) detected = "Bengali";
+            else if (/[\u0600-\u06FF]/.test(userMessage)) detected = "Urdu";
             else if (/(hai|kya|kaise|nahi|aur|mein|tum|aap)/i.test(userMessage)) detected = "Hinglish";
             else detected = "English";
         }
